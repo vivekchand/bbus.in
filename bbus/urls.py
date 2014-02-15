51c1b27d7f20c2d398c283a1290a873da0c0175f
Vivek Chand <vivekchand19@gmail.com>
2014-02-15 13:35:58 +0530

    Improved UI of bbus.in

--- bbus/urls.py
+++ bbus/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,4 +17,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'bbus.views.index', name='index'),
-)
+    url(r'^list.json$', 'bbus.views.bbus_list', name='bbus_list'),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- bbus/views.py
+++ bbus/views.py
@@ -1,10 +1,14 @@
 from django.conf import settings
 if not settings.configured:
     settings.configure()
-
+import json
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 
 def index(request):
     return render(request, 'index.html')
 
+def bbus_list(request):
+    bus_json = open('bus_json.json').read().split('\n')
+    return HttpResponse(json.dumps(bus_json), content_type="application/json")
+    
