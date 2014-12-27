06c9d9d28f829daf790fabc12cd79c8d413e97e2
Vivek Chand <vivekchand19@gmail.com>
2014-12-27 20:39:41 +0530

    added duration & distance for each hop

--- bbus/bbus_utils.py
+++ bbus/bbus_utils.py
@@ -1,4 +1,5 @@
-from BeautifulSoup import BeautifulSoup
+import requests
+
 def clean(text):
     if text == 'Route Numbers':
         return 'bus_nos'
@@ -17,6 +18,21 @@ def table2json(rows):
         for ele in value:
             json_dict[keys[c]] = ', '.join(ele.split(','))
             c+=1
+        _from = u'{} Bus Stop, Bangalore'.format(json_dict['From'])
+        _to = u'{} Bus Stop, Bangalore'.format(json_dict['To'])
+        resp = requests.get(
+            'https://maps.googleapis.com/maps/api/directions/json?'
+            'origin={origin}&destination={destination}&key={key}&'
+            'departure_time=now&mode=transit'.format(
+                origin=_from,destination=_to,
+                key='AIzaSyCNxasSekURQXrJ8eOlPEhQK8foxJ__WqE')).json()
+        if resp['status'] == 'OK' and resp['routes'] \
+                and resp['routes'][0]['legs'] and resp['routes'][0]['legs'][0]['distance']:
+            json_dict['distance'] = resp['routes'][0]['legs'][0]['distance']['text']
+            json_dict['duration'] = resp['routes'][0]['legs'][0]['duration']['text']
+        else:
+            json_dict['distance'] = None
+            json_dict['duration'] = None
         hops.append(json_dict)
     return hops
 
